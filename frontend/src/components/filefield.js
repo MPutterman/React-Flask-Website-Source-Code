507b9ea2479c8303a1a034f85ae8491fd75faa3a
michaelvandam <mikevandam@gmail.com>
2021-05-31 22:36:13 +0000

    Major update of submission.js to switch to Uniforms form generator. Added new 'FileInputField' component to work with it.

---
+++ frontend/src/components/filefield.js
@@ -0,0 +1,42 @@
+// TODO: provide a way to specify the upload type,
+// or get it directly from files[0]. I'm just leaving as
+// image/png since that was previously in the submission.js file...
+
+import React from 'react';
+import { HTMLFieldProps, connectField } from 'uniforms';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+export type FileInputFieldProps = HTMLFieldProps<string, HTMLDivElement>;
+
+function FileInput({ name, onChange, value, label, ref }: FileInputFieldProps) {
+
+  const [filename, setFilename] = React.useState('');
+
+  return (
+    <div className="FileField">
+      <label htmlFor={name}>
+        <div>{label}</div>
+        <span>
+            <Button variant='contained' component='span'>Choose file</Button>
+            <TextField disabled value={filename ? filename : 'No file chosen'} />
+        </span>
+      </label>
+      
+      <input
+        //accept="image/*" // TODO: read this from the properties if specified
+        id={name}
+        onChange={({ target: { files } }) => {
+          if (files && files[0]) {
+            setFilename(files[0].name);
+            onChange(new Blob([files[0]],{type: 'image/png',})); //URL.createObjectURL(files[0]));
+          }
+        }}
+        style={{ display: 'none' }}
+        type="file"
+      />
+    </div>
+  );
+}
+
+export default connectField(FileInput); //connectField<FileFieldProps>(FileField);
--- frontend/src/components/submission.js
+++ frontend/src/components/submission.js
@@ -1,33 +1,30 @@
+// TODO:
+// * Find a way to capture some information about the file when upload to image table
+//   such as description, filename(?), type(dark, radiation, etc...)
+// * Find a way to select previously uploaded images, not always uploading new ones
+// * Clean up state -- there is a lot that is no longer needed
+
 import React from "react"; 
-import "../App.css";
 import axios from "axios";
-import Button from "@material-ui/core/Button";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import blueGrey from "@material-ui/core/colors/blueGrey";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import {withRouter} from "react-router";
-import {Link} from 'react-router-dom';
+import { withRouter } from "react-router";
 import { backend_url } from './config';
-import Grid from "@material-ui/core/Grid";
-import Divider from "@material-ui/core/Divider";
 
-// TODO: here's a nice tutorial on adding a progress bar for uploads...
-// TODO: here's a video on using it with React-hook-form
+import LinearProgress from "@material-ui/core/LinearProgress";
 
+// Imports for automatic form generation
+import {AutoForm, AutoField, AutoFields, ErrorField, ErrorsField, SubmitField,} from 'uniforms-material';
+import FileInputField from './filefield';
+import SimpleSchema from 'simpl-schema';
+import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
+
+// TODO: Find these web pages again:
+//  --- tutorial on adding a progress bar for uploads...
+//  --- video on using it with React-hook-form
 
 class Submission extends React.Component {
   constructor(props) {
     axios.defaults.withCredentials = true
     super(props);
-    this.fileLink = React.createRef();
-    this.dataLink = React.createRef();
-    this.darkReference = React.createRef();
-    this.flatReference = React.createRef();
-    this.cerenkovReference = React.createRef();
-    this.UVReference = React.createRef();
-    this.UVFlatReference = React.createRef();
-    this.brightReference = React.createRef();
-    this.brightFlatReference = React.createRef();
     this.filenum=0
     this.state = {
       arr_files: [],
@@ -51,50 +48,63 @@ class Submission extends React.Component {
       contrast: 0,
       show_us: "About Us",
       start: false,
-      Darkname: "SampleDarkField",
-      Flatname: "SampleFlatField",
-      Cerenkovname: "SampleCerenkov",
-      Brightname: "",
-      BrightFlatname: "",
-      UVname: "",
-      UVFlatname: "",
       dataUploaded: false,
       resultsReturned: false,
       results: [[]],
       makeUpdate: 0,
       doROIs: false,
-      Dark: null,
-      Flat: null,
-      Cerenkov: null,
-      UV: null,
-      UVFlat: null,
-      Bright: null,
-      BrightFlat: null,
       ImgReturned: false,
       img: 0,
       background_corrected:'',
       name:''
       
     };
-    console.log(this.url)
+    console.log(backend_url(''))
     axios.defaults.withCredentials = true
-  }
 
-  // Get backend IP
-  // TODO: add error checking
-  get backend_ip() {
-    return process.env.REACT_APP_BACKEND_IP
-  }
+    this.formRef = null;
 
-  // Get backend port
-  // TODO: add error checking and/or default value
-  get backend_port() {
-    return process.env.REACT_APP_BACKEND_PORT
-  }
+    this.schema = new SimpleSchema ({
+      image_radiation: {
+        label: 'Radiation Image',
+        type: Blob,
+        required: false, // TODO: should be true
+      },
+      image_radiation_dark: {
+        label: 'Radiation Dark Image',
+        type: Blob,
+        required: false,
+      },
+      image_flat: {
+        label: 'Flat Image',
+        type: Blob,
+        required: false,
+      },
+      image_brightfield: {
+        label: 'Brightfield Image',
+        type: Blob,
+        required: false,
+      },
+      // TODO: REMOVE THE FIELDS BELOW... LEGACY
+      image_brightfield_flat: {
+        label: 'LEGACY: Brightfield Flat Image',
+        type: Blob,
+        required: false,
+      },
+      image_uv: {
+        label: 'LEGACY: UV Image',
+        type: Blob,
+        required: false,
+      },
+      image_uv_flat: {
+        label: 'LEGACY: UV Flat Image',
+        type: Blob,
+        required: false,
+      },
+    });
 
-  // Get url for backend server requests
-  get url() {
-    return 'http://' + this.backend_ip + ':' + this.backend_port
+    this.bridge = new SimpleSchema2Bridge(this.schema);
+
   }
 
   makeData = (arr) => {
@@ -114,7 +124,8 @@ class Submission extends React.Component {
     this.filenum = res.data.res
     this.setState({})
     console.log(this.filenum)
-    this.fileLink.current.click()
+    const {history} = this.props;
+    history.push('/analysis/' + this.filenum);
     
     
     
@@ -125,41 +136,72 @@ class Submission extends React.Component {
   };
   
   
-  onFileUpload = () => {
+  onFileUpload = (data) => {
     this.setState({ submitted: true });
-    let data = new FormData();
-    const fileblob1 = new Blob([this.state.Dark], { type: "image/png" });
-    data.append("Dark", fileblob1);
-    const fileblob2 = new Blob([this.state.Flat], { type: "image/png" });
-    data.append("Flat", fileblob2);
-    const fileblob3 = new Blob([this.state.UVFlat], { type: "image/png" });
-    data.append("UVFlat", fileblob3);
-    const fileblob4 = new Blob([this.state.UV], { type: "image/png" });
-    data.append("UV", fileblob4);
-    const fileblob5 = new Blob([this.state.Cerenkov], { type: "image/png" });
-    data.append("Cerenkov", fileblob5);
-    const fileblob6 = new Blob([this.state.Bright], { type: "image/png" });
-    data.append("Bright", fileblob6);
-    const fileblob7 = new Blob([this.state.BrightFlat], { type: "image/png" });
-    data.append("BrightFlat", fileblob7);
-    data.append('BrightName',this.state.Brightname)
-    data.append('FlatName',this.state.Flatname)
-    data.append('CerenkovName',this.state.Cerenkovname)
-    data.append('DarkName',this.state.Darkname)
-    data.append('UVName',this.state.UVname)
-    data.append('UVFlatName',this.state.UVFlatname)
-    data.append('BrightFlatName',this.state.BrightFlatname)
-    return axios
-      .post(this.url + '/time', data, {
-        headers: {
+    let formData = new FormData();
+
+    // Transform empty files
+    // TODO: fix backend so it only works with files sent...
+    // TODO: also fix backend so it doesn't need filenames...
+    if (!data.image_radiation) {
+      data.image_radiation = new Blob([null], { type: "image/png" });
+    }
+    if (!data.image_radiation_dark) {
+      data.image_radiation_dark = new Blob([null], { type: "image/png" });
+    }
+    if (!data.image_flat) {
+      data.image_flat = new Blob([null], { type: "image/png" });
+    }
+    if (!data.image_brightfield) {
+      data.image_brightfield = new Blob([null], { type: "image/png" });
+    }
+    if (!data.image_brightfield_flat) {
+      data.image_brightfield_flat = new Blob([null], { type: "image/png" });
+    }
+    if (!data.image_uv) {
+      data.image_uv = new Blob([null], { type: "image/png" });
+    }
+    if (!data.image_uv_flat) {
+      data.image_uv_flat = new Blob([null], { type: "image/png" });
+    }
+
+    // Add files to formData
+    formData.append('Cerenkov', data.image_radiation);
+    formData.append('Dark', data.image_radiation_dark);
+    formData.append('Flat', data.image_flat);
+    formData.append('Bright', data.image_brightfield);
+    formData.append('BrightFlat', data.image_brightfield_flat);
+    formData.append('UV', data.image_uv);
+    formData.append('UVFlat', data.image_uv_flat);
+
+    // Add filenames to formData
+    // TODO: fix backend so we don't need to send these
+    formData.append('BrightName','');
+    formData.append('FlatName', 'SampleFlatField');
+    formData.append('CerenkovName', 'SampleCerenkov');
+    formData.append('DarkName', 'SampleDarkField');
+    formData.append('UVName','');
+    formData.append('UVFlatName','');
+    formData.append('BrightFlatName','');
+
+    // Debugging (show what is being sent via the form)
+    //for (var pair of formData.entries()) {
+    //    console.log(pair[0]+ ', ' + pair[1]); 
+    //}
+
+    let config = {
+      headers: {
           "Content-Type": "multipart/form-data",
-        },
-      })
-      .then((res) => {
-        this.onReturnProcessed(res);
-        return res;
-      })
+      },
+    };
+
+    return axios.post(backend_url('/time'), formData, config)
+    .then((res) => {
+      this.onReturnProcessed(res);
+      return res;
+    })
   };
+
   handleSubmitClick=()=>{
     if (!this.state.submitDisabled){
       return
@@ -170,259 +212,33 @@ class Submission extends React.Component {
   render() {
     return (
         <div> 
+          <div>
+              <AutoForm
+                schema={this.bridge}
+                onSubmit={this.onFileUpload}
+                ref={ref => (this.formRef = ref)}
+              >
+                <AutoField name="image_radiation" component={FileInputField} />
+                <ErrorField name="image_radiation" />
+                <AutoField name="image_radiation_dark" component={FileInputField} />
+                <ErrorField name="image_radiation_dark" />
+                <AutoField name="image_flat" component={FileInputField} />
+                <ErrorField name="image_flat" />
+                <AutoField name="image_brightfield" component={FileInputField} />
+                <ErrorField name="image_brightfield" />
+                <p> Below are not really supported any more </p>
+                <AutoField name="image_brightfield_flat" component={FileInputField} />
+                <ErrorField name="image_brightfield_flat" />
+                <AutoField name="image_uv" component={FileInputField} />
+                <ErrorField name="image_uv" />
+                <AutoField name="image_uv_flat" component={FileInputField} />
+                <ErrorField name="image_uv_flat" />
+                <p>If you submit without choosing files, it will use sample data</p>
 
-        <h1 align="center">Click on the buttons below to choose images for upload</h1>
-
-        <Grid container direction='col' spacing={3}>
-        <Grid container direction='row' xs='12' spacing={2} >
-
-          <Grid item xs={4}>
-              <h2 align="center">Radiation images</h2>
-              <Grid container direction='column' spacing={1}>
-
-                  <Grid item>                  
-                    <input
-                      type="file"
-                      hidden
-                      ref={this.cerenkovReference}
-                      onChange={(e) => {
-                        this.setState({
-                          Cerenkovname: e.target.value.substr(
-                            e.target.value.indexOf("FAKEPATH/") + 13
-                          ),
-                          Cerenkov: e.target.files[0],
-                        });
-                      }}
-                    />
-                    <Button
-                      fullWidth
-                      color="primary"
-                      variant="contained"
-                      onClick={() => this.cerenkovReference.current.click()}
-                    >
-                      Cerenkov Image: {this.state.Cerenkovname}
-                    </Button>
-                  </Grid>
-
-                  <Grid item>
-                    <input
-                      type="file"
-                      hidden
-                      ref={this.darkReference}
-                      onChange={(e) => {
-                        this.setState({
-                          Darkname: e.target.value.substr(
-                            e.target.value.indexOf("FAKEPATH/") + 13
-                          ),
-                          Dark: e.target.files[0],
-                        });
-                      }}
-                    />
-                    <Button
-                      fullWidth
-                      color="primary"
-                      variant="contained"
-                      onClick={() => this.darkReference.current.click()}
-                    >
-                      Cerenkov Dark Image (optional): {this.state.Darkname}
-                    </Button>
-                  </Grid>
-
-
-                  <Grid item>                  
-                    <input
-                      type="file"
-                      hidden
-                      ref={this.flatReference}
-                      onChange={(e) => {
-                        this.setState({
-                          Flatname: e.target.value.substr(
-                            e.target.value.indexOf("FAKEPATH/") + 13
-                          ),
-                          Flat: e.target.files[0],
-                        });
-                      }}
-                    />
-                    <Button
-                     fullWidth
-                      color="primary"
-                      variant="contained"
-                      onClick={() => this.flatReference.current.click()}
-                    >
-                      Cerenkov Flat Image (optional): {this.state.Flatname}
-                    </Button>
-                  </Grid>
-
-                </Grid>
-            
-            </Grid>
-
-          <Grid item xs={4}>
-              <h2 align="center">Brightfield images (All optional)</h2>
-              <Grid container direction='column' spacing={1}>
-
-                  <Grid item>
-                    <input
-                      type="file"
-                      hidden
-                      ref={this.brightReference}
-                      onChange={(e) => {
-                        this.setState({
-                          Brightname: e.target.value.substr(
-                            e.target.value.indexOf("FAKEPATH/") + 13
-                          ),
-                          Bright: e.target.files[0],
-                        });
-                      }}
-                    />
-                    <Button
-                      fullWidth
-                      color="primary"
-                      variant="contained"
-                      onClick={() => this.brightReference.current.click()}
-                    >
-                      Brightfield Image: {this.state.Brightname}
-                    </Button>
-                  </Grid>
-
-                  <Grid item>
-                    <input
-                      type="file"
-                      hidden
-                      ref={this.brightFlatReference}
-                      onChange={(e) => {
-                        this.setState({
-                          brightFlatname: e.target.value.substr(
-                            e.target.value.indexOf("FAKEPATH/") + 13
-                          ),
-                          BrightFlat: e.target.files[0],
-                        });
-                      }}
-                    />
-                    <Button
-                      fullWidth
-                      color="primary"
-                      variant="contained"
-                      onClick={() => this.brightFlatReference.current.click()}
-                    >
-                      Brightfield Flat Image:{" "}
-                      {this.state.brightFlatname}
-                    </Button>
-                  </Grid>
-
-                </Grid>
-            </Grid>
-
-            <Grid item xs={4}>
-            
-              <h2 align="center">UV images (All optional)</h2>
-              <Grid container direction='column' spacing={1}>
-
-                  <Grid item>
-                    <input
-                      type="file"
-                      hidden
-                      ref={this.UVReference}
-                      onChange={(e) => {
-                        this.setState({
-                          UVname: e.target.value.substr(
-                            e.target.value.indexOf("FAKEPATH/") + 13
-                          ),
-                          UV: e.target.files[0],
-                        });
-                      }}
-                    />
-                    <Button
-                      fullWidth
-                      color="primary"
-                      variant="contained"
-                      onClick={() => this.UVReference.current.click()}
-                    >
-                      UV Image: {this.state.UVname}
-                    </Button>
-                  </Grid>
-
-                  <Grid item>
-                    <input
-                      type="file"
-                      hidden
-                      ref={this.UVFlatReference}
-                      onChange={(e) => {
-                        this.setState({
-                          UVFlatname: e.target.value.substr(
-                            e.target.value.indexOf("FAKEPATH/") + 13
-                          ),
-                          UVFlat: e.target.files[0],
-                        });
-                      }}
-                    />
-                    <Button
-                      fullWidth
-                      color="primary"
-                      variant="contained"
-                      onClick={() => this.UVFlatReference.current.click()}
-                    >
-                      UV Flat Image: {this.state.UVFlatname}
-                    </Button>
-                  </Grid>
-                </Grid>
-              
-            </Grid>
-          </Grid>
-        </Grid>
-
-        <h1 align="center">Submission options</h1>
-
-        <Grid container direction="row" spacing="{10}">
-            <Grid item xs={4}>
-                      <Button
-                        fullWidth
-                        color="primary"
-                        variant="contained"
-                        style={{
-                          //fontSize: "2vh",
-                          //position: "absolute",
-                          //marginTop: "70vh",
-                          //marginLeft: "40vw",
-                          //width: "20vw",
-                          //height: "20vh",
-                        }}
-                        onClick={this.onFileUpload}
-                        disabled={this.state.disabled}
-                      >
-                        Use Sample Data
-                      </Button>
-            </Grid>
-            <Grid item xs={4}>
-
-                    <Link
-                      type = 'hidden'
-                      ref={this.fileLink}
-                      style={{
-                        display:'none',
-                      }}
-                      to={{ pathname: '/analysis/'+this.filenum}}
-                    >click here</Link> 
-
-            </Grid>
-            <Grid item xs={4}>
-
-                      <Button
-                        fullWidth
-                        color="primary"
-                        variant="contained"
-                        onClick={this.onFileUpload}
-                        disabled={this.state.disabled}
-                      >
-                        Submit files to start analysis
-                      </Button>
-                
-              </Grid>
-            </Grid>
-
-
-            </div>
-   
+                <SubmitField>Upload Files and Start Analysis</SubmitField>
+              </AutoForm>
+          </div>
+      </div>
     );
   }
 }
