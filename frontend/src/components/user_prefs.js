b501872d77e4c2a6458fe7fc83ed4f0c8f91c28c
michaelvandam <mikevandam@gmail.com>
2021-06-14 06:21:41 +0000

    Major code cleanup

--- frontend/src/components/user_prefs.js
+++ frontend/src/components/user_prefs.js
@@ -1,12 +1,19 @@
 // TODO:
+// * Like in other forms, need a useEffect() to reset form defaults (when user prefs changes),
+//     so that after submitting form, the new values get 'locked in', and reset will return to the new saved state
+// * MAJOR: does not yet mark the Auth context as dirty when update prefs (nor updates the prefs in the session)
 // * Add preference: list of favorite plate_types
 // * Add preference: list of favorite cover_types
-// * Add option to reset preferences to site defaults?
+// * Add option to reset preferences to site defaults?  (I -think- form reset will return to state of model (from database))
 // * Should tie into the backend session and be retrieved when load sesion.
-// * Make sure caching in session handled properly (i.e. mark dirty when preferences
+// * Make sure caching in front-end Auth context handled properly (i.e. mark dirty when preferences
 //   are saved).
-// * Add cancel function
-// * Add rest of fields
+// * In the IDInputField, need more control over button labels... e.g. for images, we might like 'Choose' and 'Upload', rather
+//   than 'Choose and 'Create'....?
+// * How do we handle the case when an equipment or image, etc.. refered in someone's preference is deleted?
+// * Prevent showing preferences when no user is logged in
+// * Maybe null/undefined values should not be sent to the database...? There may be an option in simplschema
+//    to clean-up the values before validate/submit
 
 import React from "react";
 import { withRouter } from "react-router";
@@ -16,7 +23,7 @@ import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { useAuthState, useAuthDispatch, defaultPrefs } from '../contexts/auth';
+import { useAuthState, useAuthDispatch, defaultUserPrefs } from '../contexts/auth';
 import Busy from '../components/busy';
 import AlertList from '../components/alerts';
 import { callAPI } from '../components/api';
@@ -32,25 +39,15 @@ const UserPrefs = (props) => {
     const session = useAuthState();
     const dispatch = useAuthDispatch();
 
-    // TODO: Put all default values here, rather than in the schema?
-    const initialUserPrefs = {
-      "general.redirect_after_login": '/home',
-    };
+    const initialUserPrefs = defaultUserPrefs; // Defaults currently stored in auth.js
 
-    const [loading, setLoading] = React.useState(false);
+    const [loading, setLoading] = React.useState(false); 
 
     const [currentUserPrefs, setCurrentUserPrefs] = React.useState(initialUserPrefs);
-    //const [currentUser, setCurrentUser] = React.useState(initialUserState);
-    // TODO: add available images, etc...
 
     const [alert, setAlert] = React.useState({});
-    // Form hooks
-    // mode is the render mode (both onChange and onBlur)
-    // defaultValues defines how the form will be 'reset'. Fill back in with retrieved user info
-//    const {handleSubmit, reset, control} = useForm({mode: 'all', defaultValues: currentUser}); 
 
-    // TODO: how to deal with multi-level schema, and grouped inputs?
-    // Schema
+    // Schema (uses multi-level inputs for categories and keys)
     const schema = new SimpleSchema ({
         general: {
             label: 'Category - General preferences',
@@ -62,8 +59,7 @@ const UserPrefs = (props) => {
         },
         "general.timezone": {
             label: 'Time zone',
-            type: String, // TODO: are there some validators for this?
-            defaultValue: '',
+            type: String, // TODO: convert to select? or are there some predefined validators for valid timezones?
         },
         analysis: {
             label: 'Category - Analysis preferences',
@@ -72,159 +68,103 @@ const UserPrefs = (props) => {
         "analysis.default_plate": {
             label: 'Default TLC plate type',
             type: String, // plate_id
-            defaultValue: null,
         },
         "analysis.default_cover": {
             label: 'Default TLC cover type',
             type: String, // cover_id
-            defaultValue: null,
         },
         "analysis.default_equip": {
             label: 'Default equipment ID for new analysis',
             type: String, // equip_id
-            defaultValue: null,
         },
         "analysis.default_exposure_time": {
             label: 'Default exposure time (s) for new analysis',
             type: Number,
-            defaultValue: null,
         },
         "analysis.default_exposure_temp": {
             label: 'Default exposure temperature (C) for new analysis',
-            type: Number,
-            defaultValue: null,
-                // Warn if equipment doesn't support cooling?
+            type: Number, // TODO: somehow warn if equipment doesn't support cooling?
         },
         "analysis.default_use_flat_correction": {
             label: 'Turn on flat correction by default?',
             type: Boolean,
-            defaultValue: false,
         },
         "analysis.default_flat_image": {
             label: 'Default image for flat correction (if enabled)',
             type: String, // image_id
-            defaultValue: null,
         },
         "analysis.default_use_dark_correction": {
             label: 'Turn on dark correction by default?',
             type: Boolean,
-            defaultValue: false,
         },
         "analysis.default_dark_image": {
             label: 'Default image for dark correction (if enabled)',
             type: String, // image_id
-            defaultValue: null,
         },
         "analysis.default_use_bkgrd_correction": {
             label: 'Turn on background correction by default?',
             type: Boolean,
-            defaultValue: true,
         },
         "analysis.default_bkgrd_algorithm": {
             label: 'Default background correction algorithm',
-            type: String, // TODO: change to select
-            defaultValue: 'gradient',
+            type: String, // TODO: change to selector
         },
         "analysis.default_use_filter_correction": {
             label: 'Turn on filter correction by default?',
             type: Boolean,
-            defaultValue: true,
         },
         "analysis.default_filter": {
             label: 'Default filter correction',
-            type: String, // TODO: change to select
-            defaultValue: 'median 3x3',
+            type: String, // TODO: change to selector
         },
-            //default_bright_image_exposure_time: '',   // OMIT FOR NOW
-            //default_bright_image_exposure_temp: '',   // OMIT FOR NOW
-            //favorite_plate_type: [],                  // list of plate_ids
-            //favorite_cover_type: [],                  // list of cover_ids
+        //default_bright_image_exposure_time: '',   // OMIT FOR NOW
+        //default_bright_image_exposure_temp: '',   // OMIT FOR NOW
+        //favorite_plate_type: [],                  // list of plate_ids
+        //favorite_cover_type: [],                  // list of cover_ids
     }, {
         requiredByDefault: false,
     });
 
 
-
-
-    // TODO: change to callAPI
-    async function getUserPrefs(id) {
-
-/*        setLoading(true);
-        if (id) {
-            axios.get(backend_url('user/load/' + id))
-            .then((response) => {
-//                setCurrentUser(response.data);
-                setLoading(false);
-            })
-            .catch((e) => {
-                console.error("GET /user/edit/" + id + ": " + e);
-                setLoading(false);
-            });
-        } else {
-            setLoading(false);
-        }
-*/
-    }
-
-/*  
-    // Retrieve user with specified id from the database
-    // TODO: Error handling if user is not found... need to redirect to not found page
-    async function getUser(id) {
+    // TODO: should this need a backend method, or just work entirely with the session?
+    // When save prefs, need to trigger session to reload / update prefs...
+    async function loadUserPrefs() {
         setLoading(true);
-        if (id) {
-            axios.get(backend_url('user/load/' + id))
-            .then((response) => {
-                setCurrentUser(response.data);
-                setLoading(false);
-            })
-            .catch((e) => {
-                console.error("GET /user/edit/" + id + ": " + e);
-                setLoading(false);
-            });
-        } else {
-            setLoading(false);
-        }
+        setCurrentUserPrefs(session.prefs);
+        setLoading(false);
     }
-*/
 
-    // Call this upon first value of props.match.params.id (should only run once)
     React.useEffect(() => {
-        console.log("In useEffect #1"); // currentUser and availableOrganizations are updated asynchronously
-        //getUser(props.match.params.id);
-    }, [props.match.params.id]);
+        console.log("In useEffect - loading prefs"); 
+        loadUserPrefs();
+    }, [session]);
 
-    // This second useEffect is triggered whenever 'currentUser' changes (i.e. after loading from database).
-    // When triggered, it sets the defaultValues of the form to currentUser, then triggers the form to reset.
-    // This causes the form fields to fill in with the newly retrieved data in currentUser.
-    // TODO: for some reason if I try to put reset(currentUser) in the getUser function it doesn't
-    // properly reset the form...
-/*
-    React.useEffect(() => {
-        console.log("In useEffect #2 => ", currentUser); //initUser);
-        reset(userPrefs);
-    }, [userPrefs]);
-*/
+    async function onSubmit(data, e)  {
+      saveUserPrefs(data);
+    };
 
-    // Save the user information back to the database
+    // Save the formdata back to the database (note it is already in nested dict format)
+    // TODO: currently doesn't pass a user_id... maybe something we want in the future for admins?
     async function saveUserPrefs(data) {
         setLoading(true);
-        return callAPI('POST', 'user/prefs', data)
+        // Hack -- better option might be to set content-type to application/json
+        // Backend needs to un-stringify this
+        let newdata = {prefs: JSON.stringify(data)};
+        return callAPI('POST', 'api/prefs/save', newdata)
         .then((response) => {
             setAlert({severity: 'success', message: 'Preferences successfully saved'});
-            setCurrentUserPrefs(response.data);
             setLoading(false);
+            return true;
         })
         .catch((e) => {
             setAlert({severity: 'error', message: 'Error while saving preferences'});
             console.log("saveUserPrefs - exception: " + e);
             setLoading(false);
+            return false;
         });
     }
 
-    // Returns how to render the form
-
     var bridge = new SimpleSchema2Bridge(schema);
-
 
     return (
 
@@ -234,7 +174,7 @@ const UserPrefs = (props) => {
 
             <AutoForm
               schema={bridge}
-              onSubmit={saveUserPrefs}
+              onSubmit={onSubmit}
               ref={ref => (formRef = ref)}
               model={currentUserPrefs}
             >
@@ -287,9 +227,7 @@ const UserPrefs = (props) => {
 
             </AutoForm>
 
-
-
-            <Button variant='contained' onClick={() => formRef.reset()}>Reset</Button>
+            <Button variant='contained' onClick={() => formRef.reset()}>Cancel (doesn't work properly yet)</Button>
 
             <AlertList alert={alert} />
 
