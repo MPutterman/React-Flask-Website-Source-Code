39511d15efedf750dbab954c3f6a9be70bf34374
michaelvandam <mikevandam@gmail.com>
2021-06-22 03:58:52 +0000

    Code cleanup

--- frontend/src/components/busy.js
+++ frontend/src/components/busy.js
@@ -4,12 +4,9 @@
 import React from 'react';
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Backdrop from "@material-ui/core/Backdrop";
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-//import { useTheme } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 
 export const Busy = (props) => {
-
-  //const theme = useTheme();
 
   return (
     <Backdrop className={props.classes.backdrop} open={props.busy} >
