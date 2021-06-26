7b110818e001db1417a94e7ab08ea7e56647a9a3
michaelvandam <mikevandam@gmail.com>
2021-06-26 07:02:14 +0000

    Utilities for Autoforms async validators

---
+++ frontend/src/helpers/validation_utils.js
@@ -0,0 +1,23 @@
+import { callAPI } from '../components/api';
+
+// Check if object with the specified ID exists
+// TODO: need to add handling for error response or no response
+export async function id_exists (objectType, id) {
+    if (!id) return true;
+    return callAPI('GET', `api/${objectType}/exists/${id}`)
+    .then((response) => {
+        return response.data.exists;
+    });
+}
+
+// Lookup up name field for the object with the specified ID
+// TODO: add error handling if error response or no response
+export async function name_lookup(objectType, id) {
+    if (!id) return '';
+    return callAPI('GET', `api/${objectType}/name/${id}`)
+    .then((response) => {
+        return response.data.name;
+    });
+}
+
+
