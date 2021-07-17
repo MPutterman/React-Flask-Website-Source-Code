687d1d737eec915f8e718c62838aaaf127a7eb18
michaelvandam <mikevandam@gmail.com>
2021-07-17 22:09:45 +0000

    Improve look of IDInputField. Add permissions framework. Add option to 'view' current selected record

--- frontend/src/components/idfield.js
+++ frontend/src/components/idfield.js
@@ -4,16 +4,17 @@
 //
 // Usage: <IDInputField objectType=<String> selectLabel=<String> createLabel=<String> clearLabel=<string> />
 // - objectType<String> = user, image, equip, org, plate, cover
-// - selectButton<String> = text to write on the 'Select' button (select an existing item)
-// - chooseButton<String> = text to write on the 'Choose' button (create a new item)
-// - clearButton<String> = text to write o nthe 'Clear' button (clear the selection)
 // - selectTitle<String> = text to display as title on 'Select' popup
 // - createTitle<String> = text to display as title on 'Create' popup
+// - editTitle<String> = text to display as title on 'Edit' popup
 // - filter<Array> = array of dict with 'field', 'value', and 'operator' to constrain
 //     the pre-populate some fields of 'create' forms, or filters on 'select' forms
 //     * Note a special 'field' operator means to take the value from the current (surrounding) form for the field named ${value}
 
 // TODO:
+// * When 'edit', maybe should disable the OK button if the form is dirty (i.e. force user to save/cancel first)
+// * Incorporate some permissions. E.g. if not permission to edit the item, show in view mode.
+//     E.g. if not allowed to create iteams, don't show Create button.
 // * Update to support multiple selection
 // * Allow editing ID field directly (typing)? This has some complications:
 //    - We would probably want to load the name of the corresponding record,
@@ -22,10 +23,10 @@
 //      doesn't show up in the form submission. Would need to fix what is going on.
 //      (Also, right now after choosing/creating new object, and setting ID automatically,
 //       the id field becomes uneditable.)
-// * Add a 'View Details' button (popup or hover) to show details of currently selected item
 // * Add some intelligent behavior if the objectType doesn't exist, e.g. remove the button altogether?
 // * Figure out how to add a * next to label when field is required
-// * When we have an error state... make sure underlying components render in error state
+// * When we have an error state... make sure underlying components render in error state'
+// * Add a 'disabled' or 'readOnly' mode that disabled/removes buttons when only want to show the ID and Name
 
 // Main imports
 import React from 'react';
@@ -37,17 +38,28 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Box from '@material-ui/core/Box';
+import Input from '@material-ui/core/Input';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
+import IconButton from '@material-ui/core/IconButton';
+import ClearIcon from '@material-ui/icons/Clear';
+import SelectIcon from '@material-ui/icons/Search';
+import CreateIcon from '@material-ui/icons/AddCircle';
+import EditIcon from '@material-ui/icons/Edit';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 // Object-specific imports
 import ImageSelect from '../components/image_search'; // renaming default component
 import UserSelect from '../components/user_search'; // renaming default component
+import EquipSelect from '../components/equip_search'; // renaming default component
 import ImageCreate from '../components/image_edit'; // renaming default component
 import UserCreate from '../components/user_edit'; // renaming default component
-import EquipSelect from '../components/equip_search'; // renaming default component
 // import EquipCreate from '../components/equip_edit'; // renaming default component
+import ImageEdit from '../components/image_edit'; // renaming default component
+import UserEdit from '../components/user_edit'; // renaming default component
+// import EquipEdit from '../components/equip_edit'; // renaming default component
 
 export type IDInputFieldProps = HTMLFieldProps<string, HTMLDivElement>;
 
@@ -57,6 +69,8 @@ function IDInput({ name, error, onChange, value, label, ref, ...props }: IDInput
   const [nameField, setNameField] = React.useState('');
   const [openSelect, setOpenSelect] = React.useState(false);
   const [openCreate, setOpenCreate] = React.useState(false);
+  const [openEdit, setOpenEdit] = React.useState(false);
+  const [refresh, setRefresh] = React.useState(false);
   const [filter, setFilter] = React.useState([]); // filter rewriter (will use useEffect to capture initial value)
 
   React.useEffect(() => {
@@ -90,11 +104,12 @@ function IDInput({ name, error, onChange, value, label, ref, ...props }: IDInput
   }, [props.filter, form.model]); // Need to pass in form.model as a dependency
 
 
-  // Whenever value of ID field changes, lookup the name to display.
-  // This allows to uniformly obtain the name whether showing the form initially, or after
-  //  creating or selecting an object.
+  // Whenever value of ID field changes, lookup the corresponding 'name' to display.
+  // Also do this when 'refresh' flag is set. (E.g. if open the edit dialog, the user
+  // might change this field.)
   React.useEffect(() => {
-      if (value) {
+      if (value || refresh) {
+          setRefresh(false);
           name_lookup(props.objectType, value)
           .then((name) => {
               setNameField(name);
@@ -105,7 +120,20 @@ function IDInput({ name, error, onChange, value, label, ref, ...props }: IDInput
       } else {
           setNameField('');
       }
-  }, [value]);
+  }, [value, refresh]);
+
+
+  const allowEdit = () => {
+    return !!value; // Allow if ID value is not empty
+  }
+
+  const allowSelect = () => {
+    return true;
+  }
+
+  const allowCreate = () => {
+    return true;
+  }
 
 
   const onCloseSelect = (value) => {
@@ -114,6 +142,9 @@ function IDInput({ name, error, onChange, value, label, ref, ...props }: IDInput
   const onCloseCreate = (value) => {
   }
 
+  const onCloseEdit = (value) => {
+  }
+ 
   const handleOpenSelect = () => {
     setOpenSelect(true);
   };
@@ -121,7 +152,6 @@ function IDInput({ name, error, onChange, value, label, ref, ...props }: IDInput
   const handleCloseSelect = () => {
     setOpenSelect(false);
   };
-
 
   const onOKSelect = () => {
     // TODO: if multi-select, set value to new selection(s)
@@ -152,6 +182,29 @@ function IDInput({ name, error, onChange, value, label, ref, ...props }: IDInput
     setOpenCreate(false);
   };
 
+  const handleOpenEdit = () => {
+    setOpenEdit(true);
+  }
+
+  const handleCloseEdit = () => {
+    setOpenEdit(false);
+  }
+
+  const onOKEdit = () => {
+    setOpenEdit(false);
+    // ID value didn't change, but Name might have
+    setRefresh(true);
+  };
+
+  const onCancelEdit = () => {
+    setOpenEdit(false);
+    // Even if cancel, may have changed and saved Name
+    setRefresh(true);
+  };
+
+
+
+
   const handleClear = () => {
       setNameField('');
       onChange('');
@@ -159,40 +212,62 @@ function IDInput({ name, error, onChange, value, label, ref, ...props }: IDInput
 
   return (
     <div className="IDInputField">
-      <TextField id={name} value={value ? value : ''} readOnly={true} disabled={true} error={!!(error)} label={label}/>
+      <Box display="flex" flexDirection="row" fullWidth>
+      <Box width={280}>
+      <TextField size="small" id={name} value={value ? value : ''} error={!!(error)} label={label} 
+            InputProps={{
+            endAdornment:(
+            <InputAdornment position="end">
+                <IconButton size='small' onClick={handleOpenEdit} disabled={!allowEdit()}>
+                    <EditIcon />
+                </IconButton>
+                <IconButton size='small' onClick={handleOpenSelect} disabled={!allowSelect()}>
+                    <SelectIcon />
+                </IconButton>
+                <IconButton size='small' onClick={handleOpenCreate} disabled={!allowCreate()}>
+                    <CreateIcon />
+                </IconButton>
+                <IconButton size='small' onClick={handleClear}>
+                    <ClearIcon />
+                </IconButton>
+
+              </InputAdornment>
+            ),}}
+      />
+      </Box>
+      <Box pl={2}>
       <label htmlFor={name}>
-        <TextField id={name + '-name'} disabled value={nameField} error={!!(error)} label={'Name'}/>
-        <div>
-            <ButtonGroup variant='outlined'>
-                <Button onClick={handleOpenSelect}>
-                  {props.selectButton ? (
-                    <span>{props.selectButton}</span>
-                  ) : (
-                    <span>Choose</span>
-                  )}
-                </Button>
-                <Button onClick={handleOpenCreate}>
-                  {props.createButton ? (
-                    <span>{props.createButton}</span>
-                  ) : (
-                    <span>Create</span>
-                  )}
-                </Button>
-                <Button onClick={handleClear}>
-                  {props.clearButton ? (
-                    <span>{props.clearButton}</span>
-                  ) : (
-                    <span>Clear</span>
-                  )}
-                </Button>
-            </ButtonGroup>
-        </div>
+         <TextField size='small' id={name + '-name'} disabled value={nameField} error={!!(error)} label={'Name'}/>
       </label>
+      </Box>
+      </Box>
       
+      <Dialog fullWidth open={openEdit} onClose={handleCloseEdit} >
+        <DialogTitle id="dialog-edit">
+            {props.editTitle ? ( <span>{props.editTitle}</span> ) : ( <span>Edit record</span> )}
+        </DialogTitle>
+        <DialogContent>
+            {{
+                'user': <UserEdit object_id={value} onSave={setTemporaryModel} {...props} />,
+                'image': <ImageEdit object_id={value} onSave={setTemporaryModel} {...props} />,
+                //'equip': <EquipEdit object_id={value} onSave={setTemporaryModel} {...props} />,
+                'default': <></>,
+            } [props.objectType || 'default'] }     {/* Use || <Component /> if need 'default' */}
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color="primary" /* not disabled */ onClick={onOKEdit}>
+            OK
+          </Button>
+          <Button variant="contained" onClick={onCancelEdit} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+
 
       <Dialog fullWidth open={openSelect} onClose={handleCloseSelect} >
         <DialogTitle id="dialog-select">
-            {props.selectTitle ? ( <span>{props.selectTitle}</span> ) : ( <span>'Select an item'</span> )}
+            {props.selectTitle ? ( <span>{props.selectTitle}</span> ) : ( <span>Select record</span> )}
         </DialogTitle>
         <DialogContent>
             {{
@@ -203,18 +278,21 @@ function IDInput({ name, error, onChange, value, label, ref, ...props }: IDInput
             } [props.objectType || 'default'] }     {/* Use || <Component /> if need 'default' */}
         </DialogContent>
         <DialogActions>
+          <Button variant="contained" color="primary"
+                  disabled={temporaryModel ? !temporaryModel.id : true} // Only show 'OK' button if a row is selected
+                  onClick={onOKSelect}
+          >
+            OK
+          </Button>
           <Button variant="contained" onClick={onCancelSelect} color="primary">
             Cancel
-          </Button>
-          <Button variant="contained" onClick={onOKSelect} color="primary">
-            OK
           </Button>
         </DialogActions>
       </Dialog>
 
       <Dialog fullWidth open={openCreate} onClose={handleCloseCreate} >
         <DialogTitle id="dialog-select">
-            {props.createTitle ? ( <span>{props.createTitle}</span> ) : ( <span>'Create an item'</span> )}
+            {props.createTitle ? ( <span>{props.createTitle}</span> ) : ( <span>Create new record</span> )}
         </DialogTitle>
         <DialogContent>
             {{
@@ -225,11 +303,14 @@ function IDInput({ name, error, onChange, value, label, ref, ...props }: IDInput
             } [props.objectType || 'default'] }     {/* Use || <Component /> if need 'default' */}
         </DialogContent>
         <DialogActions>
+          <Button variant="contained" color="primary"
+                  disabled={temporaryModel ? !temporaryModel.id : true} // Only show 'OK' button if ID is set (i.e. record saved)
+                  onClick={onOKCreate}
+          >
+            OK
+          </Button>
           <Button variant="contained" onClick={onCancelCreate} color="primary">
             Cancel
-          </Button>
-          <Button variant="contained" onClick={onOKCreate} color="primary">
-            OK
           </Button>
         </DialogActions>
       </Dialog>
