f719e33b5a8285a46a5618935edda1a1637f9ece
michaelvandam <mikevandam@gmail.com>
2021-08-05 22:18:28 +0000

    Add support for additional routes/views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,10 @@
 //TODO:
 // * Consider creating a config file to store routes. This could then also be used for menu generation.
 // * Add protection of routes for logged in users
-// * Upgrade to router v6 syntax: https://reacttraining.com/blog/react-router-v6-pre/
+// * Any way to put the useRouter routing in here?  (Also the props.match.params.<xyz>, since it is 
+//     not needed if component used alone).  Maybe be some bugs for embedded
+//     pages the way it is now (should strip out props.params.match in those cases...)
+// * Future: Upgrade to router v6 syntax: https://reacttraining.com/blog/react-router-v6-pre/
 //
 // References:
 // https://www.npmjs.com/package/material-ui-confirm (confirm provider documentation)
@@ -50,14 +53,11 @@ import NotFound from './components/notfound';
 import Analysis from './components/analysis';
 import Database from './components/database'
 import Submission from './components/submission'
-import { UserView, EquipView } from './components/object_view';
-import UserEdit from './components/user_edit';
-import { UserSearch, EquipSearch, ImageSearch, AnalysisSearch } from "./components/object_search";
+import { UserView, OrgView, EquipView, PlateView, CoverView, ImageView, AnalysisView } from './components/object_view';
+import { UserSearch, OrgSearch, EquipSearch, PlateSearch, CoverSearch, ImageSearch, AnalysisSearch } from "./components/object_search";
 import UserLogin from "./components/user_login"; 
 import UserPrefs from "./components/user_prefs";
-import ImageEdit from './components/image_edit';
-//import EquipEdit from './components/equip_edit';
-
+import { UserEdit, UserRegister, OrgEdit, EquipEdit, PlateEdit, CoverEdit, ImageEdit, /*AnalysisEdit*/ } from './components/object_edit';
 
 // Wrap a portion of the app so we can access the needed contexts (i.e. Auth)
 const App = (props) => {
@@ -85,34 +85,49 @@ const AppWrapped = (props) => {
           <AlertList>
           <ErrorHandler>
             <Switch>
-{/*                <Route path = '/analysis/search' component={Database}/> */}
-{/*                <Route path='/start' component={Start}/> */}
-{/* TODO: Need to find a way to force a refresh for /analysis/new... or create a new component? 
-      currently if in /analysis/edit/<id>, then go to /analysis/new, all the data/state is still there */}
+
                 <PrivateRoute path='/analysis/new'><Analysis new={true} /></PrivateRoute>
                 <PrivateRoute path='/analysis/edit/:id'><Analysis /></PrivateRoute>
                 <PrivateRoute path='/analysis/search'><AnalysisSearch /></PrivateRoute>
                 <PrivateRoute path='/analysis/:analysis_id'><Analysis /></PrivateRoute>
-                <PrivateRoute path='/image/search'><ImageSearch /></PrivateRoute>
-                <PrivateRoute path='/image/new'><ImageEdit new={true} /></PrivateRoute>
-                <PrivateRoute path='/image/edit/:id'><ImageEdit /></PrivateRoute>
+
+
+                <PrivateRoute path='/org/view/:id'><OrgView /></PrivateRoute>
+                <PrivateRoute path='/org/new'><OrgEdit create={true} /></PrivateRoute>
+                <PrivateRoute path='/org/edit/:id'><OrgEdit /></PrivateRoute>
+                <PrivateRoute path='/org/search'><OrgSearch /></PrivateRoute>
+
+                <PrivateRoute path='/equip/view/:id'><EquipView /></PrivateRoute>
+                <PrivateRoute path='/equip/new'><EquipEdit create={true} /></PrivateRoute>
+                <PrivateRoute path='/equip/edit/:id'><EquipEdit /></PrivateRoute>
                 <PrivateRoute path='/equip/search'><EquipSearch /></PrivateRoute>
-                {/*
-                <PrivateRoute path='equip/new'><EquipEdit new={true} /></PrivateRoute>
-                <PrivateRoute path='equip/edit/:id'><EquipEdit /></PrivateRoute>
-                */}
+
+                <PrivateRoute path='/plate/view/:id'><PlateView /></PrivateRoute>
+                <PrivateRoute path='/plate/new'><PlateEdit create={true} /></PrivateRoute>
+                <PrivateRoute path='/plate/edit/:id'><PlateEdit /></PrivateRoute>
+                <PrivateRoute path='/plate/search'><PlateSearch /></PrivateRoute>
+
+                <PrivateRoute path='/cover/view/:id'><CoverView /></PrivateRoute>
+                <PrivateRoute path='/cover/new'><CoverEdit create={true} /></PrivateRoute>
+                <PrivateRoute path='/cover/edit/:id'><CoverEdit /></PrivateRoute>
+                <PrivateRoute path='/cover/search'><CoverSearch /></PrivateRoute>
+
+                <PrivateRoute path='/image/view/:id'><ImageView /></PrivateRoute>
+                <PrivateRoute path='/image/new'><ImageEdit create={true} /></PrivateRoute>
+                <PrivateRoute path='/image/edit/:id'><ImageEdit /></PrivateRoute>
+                <PrivateRoute path='/image/search'><ImageSearch /></PrivateRoute>
+
                 <Route exact path='/'><Home /></Route>
                 <Route exact path='/home'><Home /></Route>
                 <Route exact path='/contact'><Contact /></Route>
                 <PrivateRoute exact path='/about'><About /></PrivateRoute>
+                <PrivateRoute path='/user/view/:id'><UserView /></PrivateRoute>
+                <Route exact path='/user/register'><UserRegister create={true}/></Route> 
                 <PrivateRoute exact path='/user/edit/:id'><UserEdit /></PrivateRoute>
-                <Route exact path='/user/register'><UserEdit register={true} /></Route> 
                 <PrivateRoute exact path='/user/change_password/:id'><UserEdit change_password={true} /></PrivateRoute>
                 <Route exact path='/user/login'><UserLogin /></Route>
                 <PrivateRoute path='/user/search'><UserSearch /></PrivateRoute>
                 <PrivateRoute exact path='/user/prefs'><UserPrefs /></PrivateRoute>
-                <PrivateRoute path='/user/view/:id'><UserView /></PrivateRoute>
-                <PrivateRoute path='/equip/view/:id'><EquipView /></PrivateRoute>
 
                 <Route component={NotFound} status={StatusCodes.NOT_FOUND} /> 
             </Switch>
