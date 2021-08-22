79426ccc8c3a50a1159534de5c45400580bf8686
michaelvandam <mikevandam@gmail.com>
2021-08-22 05:10:49 +0000

    clean up imports

--- frontend/src/components/user_favorites.js
+++ frontend/src/components/user_favorites.js
@@ -10,26 +10,8 @@
 
 import React from "react";
 import { withRouter } from "react-router";
-import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import ButtonGroup from "@material-ui/core/ButtonGroup";
-import Accordion from "@material-ui/core/Accordion";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { useAuthState, useAuthDispatch, defaultUserPrefs, authRefreshSession } from '../contexts/auth';
-import { useConfigState } from '../contexts/config';
-import Busy from '../components/busy';
-import { useAlerts } from '../contexts/alerts';
-import { callAPI } from '../helpers/api';
-import {AutoForm, AutoField, AutoFields, ErrorField, ErrorsField, SubmitField,} from 'uniforms-material';
-import { id_exists } from '../helpers/validation_utils';
-import SimpleSchema from 'simpl-schema';
-import { SimpleSchema2Bridge } from 'uniforms-bridge-simple-schema-2';
-import IDInputField from '../components/idfield';
-//import TimezoneSelect, { i18nTimezones } from 'react-timezone-select';
-import InputAdornment from '@material-ui/core/InputAdornment';
+import { authRefreshSession } from '../contexts/auth';
 import { UserSearch, OrgSearch, EquipSearch, PlateSearch, CoverSearch, ImageSearch, AnalysisSearch } from '../components/object_search';
 
 
